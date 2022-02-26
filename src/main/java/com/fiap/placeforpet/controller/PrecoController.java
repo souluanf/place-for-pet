2ffafb9a0ce4f564478df207fea2b17391be1419
carlos jr <cjuniordanko@gmail.com>
2022-02-26 14:30:24 -0300

    criacao controller

---
+++ src/main/java/com/fiap/placeforpet/controller/PrecoController.java
@@ -0,0 +1,9 @@
+package com.fiap.placeforpet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/preco")
+public class PrecoController {
+}
